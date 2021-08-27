ab9a8ecf904711df3d6b08aedba62ad4c085fb06
MD Hridoy Hossain <HridoyMrhn@github.com>
2021-08-27 12:23:37 +0600

    First Rays Home Page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('.banner-slider').slick({
+        dots: false,
+        prevArrow: false,
+        nextArrow: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+    });      
+});
